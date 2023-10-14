3cd35f471902bccdfbbb0a2994fe34fd9eafff9a
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2023-10-14 16:09:08 +0300

    Mason

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,4 @@ local keymap = vim.keymap
 
 keymap.set("n", "<space>o", "o<ESC>k")
 keymap.set("n", "<space>O", "O<ESC>j")
+keymap.set("t", "<C-space>", "<C-\\><C-n>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,12 @@
 return {
+    {
+        'williamboman/mason-lspconfig.nvim',   
+        lazy = false,
+        dependencies = {"williamboman/mason.nvim"},
+        config = function()
+            require("mason-lspconfig").setup()
+        end
+    },
     'bkad/CamelCaseMotion',
 
     -- Coloscheme
