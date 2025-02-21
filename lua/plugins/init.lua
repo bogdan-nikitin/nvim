7efdf7f478dc8350161b2cd732715b4f8c4e6000
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2025-02-21 13:30:35 +0300

    Go suport

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -174,10 +174,22 @@ return {
         end
     },
     {
-        'L3MON4D3/LuaSnip',
+        "ray-x/go.nvim",
+        dependencies = {  -- optional packages
+            "ray-x/guihua.lua",
+            "neovim/nvim-lspconfig",
+            "nvim-treesitter/nvim-treesitter",
+        },
         config = function()
-            require("luasnip.loaders.from_snipmate").lazy_load()
-        end
+            require("go").setup {
+                lsp_cfg = true,
+                luasnip = true,
+                lsp_keymaps = false
+            }
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
     }
 }
 
